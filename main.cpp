9abbf53eb8fc7a4bd36da9115d2740bddae3e406
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-07-27 11:37:36 -0600

    se agrego el metood lector en el main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,35 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
-int main(){
+using namespace std;
+
+int main()
+{
+    string *arrayString = new string[39151187];
+    string nombreArchivo = "datos.txt";
+    lector(arrayString, nombreArchivo);
+
+    //llenar arbolRojoNegro nuestro
+
+    //llenar arbolRojoNegro STL
+
+    //llenar vector STL
+
+    //guardamos en archivo de texto
     return 0;
+}
+
+void lector(string *arrayString, string nombreArchivo)
+{
+    string word;
+    ifstream file;
+    file.open(nombreArchivo);
+    unsigned int counter = 0;
+    while (file >> word)
+    {
+        arrayString[counter] = word;
+        ++counter;
+    }
 }
